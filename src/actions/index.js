2e4f026afa541e477e5442f860643d905f97a882
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-06-26 12:41:16 -0400

    sign up a user complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
 
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 
-export const LOGIN_START = 'LOGIN_START';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const SIGNUP_START = "SIGNUP_START";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_FAIL = "SIGNUP_FAIL";
 
+const URLEndpoint = "http://localhost:5000";
 
 export const login = creds => dispatch => {
-    dispatch({ type: LOGIN_START });
-    return axios.post('https://steemly-org-chart.herokuapp.com/', creds)
-        .then(res => {
-            localStorage.setItem('token', res.data.payload);
-            dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload})    
-            this.props.history.push('/home');
-        })
-}
+  dispatch({ type: LOGIN_START });
+  return axios.post(`${URLEndpoint}`, creds).then(res => {
+    localStorage.setItem("token", res.data.payload);
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
+    this.props.history.push("/home");
+  });
+};
+
+export const signUp = creds => dispatch => {
+  console.log(creds);
+  dispatch({ type: SIGNUP_START });
+
+  return axios
+    .post(`${URLEndpoint}/api/register`, creds)
+    .then(res => {
+      console.log(res.data);
+      // this.props.history.push("/user/login");
+      return dispatch({ type: SIGNUP_SUCCESS });
+    })
+    .catch(err => {
+      console.log(err);
+      return dispatch({
+        type: SIGNUP_FAIL
+      });
+    });
+};
--- src/components/Login-SignUp/SignUp.js
+++ src/components/Login-SignUp/SignUp.js
@@ -1,140 +1,169 @@
-import React from 'react';
-import styled from 'styled-components';
-import { connect } from 'react-redux';
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
 
-import { login } from '../../actions';
-import { history } from '../../helpers/history';
+import { login, signUp } from "../../actions";
+import { history } from "../../helpers/history";
 
 const FormContainer = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 
 const SignUpForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    margin: 50px 35% 0px 35%;
-    width: 50%;
-    background-color: white;
-    height: 310px;
-    padding: 25px 1% 0px 1%;
-    border-radius: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  margin: 50px 35% 0px 35%;
+  width: 50%;
+  background-color: white;
+  height: 310px;
+  padding: 25px 1% 0px 1%;
+  border-radius: 10px;
+`;
 
 const Input = styled.input`
-    padding: 10px;
-    margin-top: 10px;
-    border-radius: 10px;
-    border: 1px solid grey;
-`
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 10px;
+  border: 1px solid grey;
+`;
 
 const Select = styled.select`
-    padding: 10px;
-    margin-top: 10px;
-    border-radius: 10px;
-    border: 1px solid grey;
-    :invalid { 
-        color: gray; 
-    }
-    :first{
-        color: grey;
-    }
-`
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 10px;
+  border: 1px solid grey;
+  :invalid {
+    color: gray;
+  }
+  :first {
+    color: grey;
+  }
+`;
 
 const Button = styled.button`
-    margin-top: 10px;
-    border-radius: 10px;
-    background-color: #db6450;
-    color: white;
-    padding: 10px;
-    border: 1px solid grey;
-`
+  margin-top: 10px;
+  border-radius: 10px;
+  background-color: #db6450;
+  color: white;
+  padding: 10px;
+  border: 1px solid grey;
+`;
 
 class SignUp extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            username: '',
-            name:'',
-            email: '',
-            password: '',
-        }
-    }
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value});
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      name: "",
+      email: "",
+      password: ""
     };
+  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    handleLogin = e => {
-        // e.preventDefault();
-        const email = this.state.email;
-        localStorage.setItem('email', email);
-        this.props.history.push('/company');
-    };
-    render(){
-        return(
-            <FormContainer>
-                <SignUpForm>
-                    <Select 
-                        required 
-                        name='company_type' 
-                        value={this.state.company_type} 
-                        onChange={this.handleChange}>
-                            <option value="" disabled selected>Select your Industry...</option>
-                            <option value="agribusiness">Agribusiness</option>
-                            <option value="communications-electronics">Communications & Electronic Goods</option>
-                            <option value="construction">Construction</option>
-                            <option value="defense">Defense</option>
-                            <option value="energy-natural-resources">Energy & Natural Resources</option>
-                            <option value="finance-insurance-real-estate">Finance, Insurance, & Real Estate</option>
-                            <option value="food-beverage">Food & Beverage</option>
-                            <option value="health">Health</option>
-                            <option value="labor">Labor</option>
-                            <option value="law-lobbyists">Law & Lobbyists</option>
-                            <option value="misc">Misc Business</option>
-                            <option value="other">Other</option>
-                            <option value="technology">Technology</option>
-                            <option value="transportation">Transportation</option>
-                    </Select>
-                    <Input
-                        placeholder="Username"
-                        onChange={this.handleChange}
-                        type='text'
-                        value={this.state.username}
-                        name='username'
-                    />
-                    <Input
-                        placeholder="Full Name"
-                        onChange={this.handleChange}
-                        type='text'
-                        value={this.state.name}
-                        name='name'
-                    />
-                    <Input
-                        placeholder="Email"
-                        onChange={this.handleChange}
-                        type='text'
-                        value={this.state.email}
-                        name='email'
-                    />
-                    <Input
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                        type='text'
-                        value={this.state.password}
-                        name='password'
-                    />                
-
-                    <Button onClick={this.handleLogin}>Register</Button>
-                </SignUpForm>
-            </FormContainer>
-        )
+  handleLogin = e => {
+    e.preventDefault();
+    // const email = this.state.email;
+    // localStorage.setItem("email", email);
+    // this.props.history.push("/company");
+    if (
+      !this.state.username ||
+      !this.state.name ||
+      !this.state.email ||
+      !this.state.password
+    ) {
+      console.log("enter the right stuff bruh!!");
+    } else {
+      const newUser = {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password,
+        full_name: this.state.name
+      };
+      this.props.signUp(newUser);
+      this.props.history.push("/user/login");
     }
+  };
+  render() {
+    return (
+      <FormContainer>
+        <SignUpForm>
+          <Select
+            required
+            name="company_type"
+            value={this.state.company_type}
+            onChange={this.handleChange}
+          >
+            <option value="" disabled selected>
+              Select your Industry...
+            </option>
+            <option value="agribusiness">Agribusiness</option>
+            <option value="communications-electronics">
+              Communications & Electronic Goods
+            </option>
+            <option value="construction">Construction</option>
+            <option value="defense">Defense</option>
+            <option value="energy-natural-resources">
+              Energy & Natural Resources
+            </option>
+            <option value="finance-insurance-real-estate">
+              Finance, Insurance, & Real Estate
+            </option>
+            <option value="food-beverage">Food & Beverage</option>
+            <option value="health">Health</option>
+            <option value="labor">Labor</option>
+            <option value="law-lobbyists">Law & Lobbyists</option>
+            <option value="misc">Misc Business</option>
+            <option value="other">Other</option>
+            <option value="technology">Technology</option>
+            <option value="transportation">Transportation</option>
+          </Select>
+          <Input
+            placeholder="Username"
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.username}
+            name="username"
+          />
+          <Input
+            placeholder="Full Name"
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.name}
+            name="name"
+          />
+          <Input
+            placeholder="Email"
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.email}
+            name="email"
+          />
+          <Input
+            placeholder="Password"
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.password}
+            name="password"
+          />
+
+          <Button onClick={this.handleLogin}>Register</Button>
+        </SignUpForm>
+      </FormContainer>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return{
-        error: state.error,
-        loggingIn: state.loggingIn
-    }
-}
+  return {
+    error: state.error,
+    loggingIn: state.loggingIn
+  };
+};
 
-export default connect(mapStateToProps, { login, history })(SignUp);
+export default connect(
+  mapStateToProps,
+  { login, history, signUp }
+)(SignUp);
